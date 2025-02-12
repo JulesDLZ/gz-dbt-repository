3daa7166dc259908114b8a5461c24454dd32e290
Julien Delhez <julien.nicolas.delhez@gmail.com>
2025-02-12 11:17:47 +0000

    challenge 4 fin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,18 +1,22 @@
 WITH sales_data AS (
- select
+    SELECT
+        orders_id,
+        date_date,
         products_id,
         revenue,
         quantity,
         purchase_price
- from {{ ref('stg_gz_raw_data__raw_gz_sales') }}
- LEFT JOIN {{ ref('stg_gz_raw_data__raw_gz_product') }}
- USING (products_id)
- )
+    FROM {{ ref('stg_gz_raw_data__raw_gz_sales') }}
+    LEFT JOIN {{ ref('stg_gz_raw_data__raw_gz_product') }}
+    USING (products_id)
+)
 SELECT
+    orders_id,
+    date_date,
     products_id,
     revenue,
     quantity,
     purchase_price,
-    ROUND ((quantity * purchase_price),2) AS purchase_cost,
-    ROUND ((revenue - (quantity * purchase_price)),2) AS margin
+    ROUND((quantity * purchase_price), 2) AS purchase_cost,
+    ROUND((revenue - (quantity * purchase_price)), 2) AS margin
 FROM sales_data
