944889632943d38fe3ba9d9266afdc83ef9444df
Julien Delhez <julien.nicolas.delhez@gmail.com>
2025-02-12 13:26:10 +0000

    challenge 2

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -9,7 +9,7 @@ SELECT
      ,s.shipping_fee
      ,s.logcost
      ,s.ship_cost
- FROM {{ ref('int_sales_margin') }} o
+ FROM {{ ref('int_orders_margin') }} o
  LEFT JOIN {{ ref('stg_gz_raw_data__raw_gz_ship') }} s
  USING(orders_id)
  ORDER BY orders_id desc
